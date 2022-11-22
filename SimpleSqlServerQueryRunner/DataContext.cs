4a07df735338597b49229f510eb8e2b9ccca5ab9
sajjadhosseinpoor <sajjad@grannos.com.tr>
2022-11-22 23:17:27 +0300

    Add project files.

---
+++ SimpleSqlServerQueryRunner/DataContext.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Data.Entity.Core.Mapping;
+using System.Text.Json.Serialization;
+
+namespace SimpleSqlServerQueryRunner
+{
+    public class DataContext : DbContext
+    {
+        private string cn = "";
+        public DataContext(string connectionString)
+        {
+            cn = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to sql server with connection string from app settings
+
+            if (!cn.ToLower().Contains("trustcervercertificate"))
+            {
+                cn = "TrustServerCertificate=True;" + cn;
+            }
+
+            options.UseSqlServer(cn);
+        }
+
+        public bool OpenConnection()
+        {
+            try
+            {
+                Database.OpenConnection();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public string[] GetTables()
+        {
+            return Database.SqlQueryRaw<string>("SELECT * FROM sys.Tables").ToArray<string>();
+        }
+
+        public string[] GetTableSchema(string tableName)
+        {
+            return Database.SqlQueryRaw<string>("select CONCAT(COLUMN_NAME, '(', DATA_TYPE,')') from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='" + tableName + "'").ToArray<string>();
+        }
+
+        internal List<string> Run(string sql)
+        {
+            if (sql.ToLower().StartsWith("select"))
+            {
+                return Database.SqlQueryRaw<string>(sql + "\nFOR JSON AUTO;").ToList();
+            }
+            else
+            {
+                string msg = "";
+                try
+                {
+                    msg = "Affect rows count: " + Database.ExecuteSqlRaw(sql);
+                }
+                catch (Exception ex)
+                {
+                    msg = "Error" + ex.Message;
+                }
+                return new List<string> { { msg } };
+            }
+        }
+    }
+}
---
+++ SimpleSqlServerQueryRunner/Program.cs
@@ -0,0 +1,65 @@
+﻿using SimpleSqlServerQueryRunner;
+
+DataContext d;
+while (true)
+{
+    SystemWrite("Connection String:\n");
+    var server = Console.ReadLine();
+
+    d = new DataContext(server);
+
+
+    SystemWrite("Connecting..... please wait!");
+    if (!d.OpenConnection())
+    {
+        SystemWrite("DB is not available!\n\n");
+    }
+    else
+    {
+        SystemWrite("Connected!");
+        Console.ResetColor();
+        Thread.Sleep(2000);
+        Console.Clear();
+        break;
+    }
+
+}
+while (true)
+{
+
+    SystemWrite("\nWrite Query:");
+
+    var sql = Console.ReadLine();
+
+    if (sql.Equals("tables"))
+    {
+        d.GetTables().ToList().ForEach(q =>
+        {
+            Console.Write(q + '\t');
+        });
+        continue;
+    }
+    else if (sql.StartsWith("table ") && sql.Split(' ').Count() == 2)
+    {
+        d.GetTableSchema(sql.Split(' ')[1]).ToList().ForEach(q =>
+        {
+            Console.Write(q+'\t');
+        });
+        continue;
+    }
+
+    var result = d.Run(sql);
+
+    result.ForEach(q => Console.WriteLine(q));
+
+}
+
+
+
+void SystemWrite(string msg)
+{
+    Console.BackgroundColor = ConsoleColor.Blue;
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine(msg);
+    Console.ResetColor();
+}
